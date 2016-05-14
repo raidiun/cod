f36d392e3e4c88210ff8c4014450c0f5313f877f
raidiun <r.95clarke@gmail.com>
2016-05-14 23:00:22 +0100

    Switch crypto/* to ERRORMSG
    Using ERRORMSG for errors and DEBUGMSG for information. This way, can still get error messages with DEBUG turned off.

--- src/crypto/crypto_kiv.c
+++ src/crypto/crypto_kiv.c
@@ -7,15 +7,15 @@
 #define KIV_PADDING RSA_PKCS1_OAEP_PADDING
 
 int encryptKIV(SymKey_t* key, SymIV_t* iv, PubKey_t pubKey, uint8_t** encBytes) {
-	if( key == NULL )      { DEBUGMSG("encryptKIV: key is NULL\r\n");      return DHTD_ERROR; }
-	if( iv == NULL )       { DEBUGMSG("encryptKIV: iv is NULL\r\n");       return DHTD_ERROR; }
-	if( pubKey == NULL )   { DEBUGMSG("encryptKIV: pubKey is NULL\r\n");   return DHTD_ERROR; }
-	if( encBytes == NULL ) { DEBUGMSG("encryptKIV: encBytes is NULL\r\n"); return DHTD_ERROR; }
+	if( key == NULL )      { ERRORMSG("encryptKIV: key is NULL\r\n");      return DHTD_ERROR; }
+	if( iv == NULL )       { ERRORMSG("encryptKIV: iv is NULL\r\n");       return DHTD_ERROR; }
+	if( pubKey == NULL )   { ERRORMSG("encryptKIV: pubKey is NULL\r\n");   return DHTD_ERROR; }
+	if( encBytes == NULL ) { ERRORMSG("encryptKIV: encBytes is NULL\r\n"); return DHTD_ERROR; }
 	
 	int err = 0;
 	
 	(*encBytes) = malloc(sizeof(uint8_t)*DHTD_ENC_KIVPAIR_BYTE_LENGTH);
-	if( (*encBytes) == NULL ) { DEBUGMSG("encryptKIV: NULL pointer allocated to encBytes\r\n"); return DHTD_ERROR; }
+	if( (*encBytes) == NULL ) { ERRORMSG("encryptKIV: NULL pointer allocated to encBytes\r\n"); return DHTD_ERROR; }
 
 	uint8_t kivPair[KIV_PAIR_LENGTH];
 	memcpy( &(kivPair[0]), key->data, DHTD_SYMKEY_BYTE_LENGTH );
@@ -24,7 +24,7 @@ int encryptKIV(SymKey_t* key, SymIV_t* iv, PubKey_t pubKey, uint8_t** encBytes) 
 	err = RSA_public_encrypt(KIV_PAIR_LENGTH, (unsigned char*) kivPair, (unsigned char*) (*encBytes), (RSA*) pubKey, KIV_PADDING);
 	if( err == -1 ) {
 		int rsaErr = ERR_peek_last_error();
-		DEBUGMSG("encryptKIV: Error in RSA_public_encrypt (%i: %s)\r\n",rsaErr,ERR_error_string(rsaErr,NULL));
+		ERRORMSG("encryptKIV: Error in RSA_public_encrypt (%i: %s)\r\n",rsaErr,ERR_error_string(rsaErr,NULL));
 		return DHTD_ERROR;
 		}
 	
@@ -32,10 +32,10 @@ int encryptKIV(SymKey_t* key, SymIV_t* iv, PubKey_t pubKey, uint8_t** encBytes) 
 	}
 
 int decryptKIV(uint8_t* encBytes, SecKey_t secKey, SymKey_t* key, SymIV_t* iv) {
-	if( encBytes == NULL ) { DEBUGMSG("decryptKIV: encBytes is NULL\r\n"); return DHTD_ERROR; }
-	if( secKey == NULL )   { DEBUGMSG("decryptKIV: secKey is NULL\r\n");   return DHTD_ERROR; }
-	if( key == NULL )      { DEBUGMSG("decryptKIV: key is NULL\r\n");      return DHTD_ERROR; }
-	if( iv == NULL )       { DEBUGMSG("decryptKIV: iv is NULL\r\n");       return DHTD_ERROR; }
+	if( encBytes == NULL ) { ERRORMSG("decryptKIV: encBytes is NULL\r\n"); return DHTD_ERROR; }
+	if( secKey == NULL )   { ERRORMSG("decryptKIV: secKey is NULL\r\n");   return DHTD_ERROR; }
+	if( key == NULL )      { ERRORMSG("decryptKIV: key is NULL\r\n");      return DHTD_ERROR; }
+	if( iv == NULL )       { ERRORMSG("decryptKIV: iv is NULL\r\n");       return DHTD_ERROR; }
 	
 	int err = 0;
 	
@@ -44,7 +44,7 @@ int decryptKIV(uint8_t* encBytes, SecKey_t secKey, SymKey_t* key, SymIV_t* iv) {
 	err = RSA_private_decrypt(DHTD_ENC_KIVPAIR_BYTE_LENGTH, (unsigned char*) encBytes, (unsigned char*) kivPair, (RSA*) secKey, KIV_PADDING);
 	if( err == -1 ) {
 		int rsaErr = ERR_peek_last_error();
-		DEBUGMSG("decryptKIV: Error in RSA_private_decrypt (%i: %s)\r\n",rsaErr,ERR_error_string(rsaErr,NULL));
+		ERRORMSG("decryptKIV: Error in RSA_private_decrypt (%i: %s)\r\n",rsaErr,ERR_error_string(rsaErr,NULL));
 		return DHTD_ERROR;
 		}
 
